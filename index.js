f66c97beb99feed8e319dc192113a2f2f8dad833
Steven <svengra01@estudiantec.cr>
2022-05-20 22:41:20 -0600

    Web Scrapping

--- index.js
+++ index.js
@@ -4,13 +4,14 @@ dotenv.config({path: '.env-local'}) // Para usar las variables de entorno
 const PORT          = process.env.PORT || '3000'
 const app           = express()
 const searchRouter  = require('./routes/search')
+const path          = require('path')
 
 app.use(express.json())
 app.use(express.urlencoded({extended:false}))
 
 // Rutas de la pagina
 app.get('/', (request, response) => {
-    response.status(200).send("Pagina de inicio")
+    response.sendFile(path.resolve(__dirname, 'index.html'))
 })
 
 app.use('/search', searchRouter)
